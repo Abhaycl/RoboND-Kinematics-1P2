d7be4369075a44b8c20f60412ce00ab7bc23ec72
Abhay <abhaycl@hotmail.com>
2018-04-19 03:00:54 +0200

    Add files via upload

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -19,37 +19,35 @@ from mpmath import *
 from sympy import *
 
 
-# Define Modified DH Transformation matrix
-def HTF_Matrix(joint, twist, length, offset):
-    HTF = Matrix([[            cos(joint),            -sin(joint),           0,             length],
-                  [ sin(joint)*cos(twist), cos(joint1)*cos(twist), -sin(twist), -sin(twist)*offset],
-                  [ sin(joint)*sin(twist), cos(joint1)*sin(twist),  cos(twist),  cos(twist)*offset],
-                  [                     0,                      0,           0,                  1]])
+# Definition of the homogeneous transformation matrix
+def HTF_Matrix(alpha, a, d, q):
+    HTF = Matrix([[            cos(q),           -sin(q),           0,             a],
+                  [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                  [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                  [                 0,                 0,           0,             1]])
     return HTF
 
-
+# Definition of the functions for the homogeneous transformation matrices of the rotations around x, y, z passing a specific angle
 # Rotation (roll)
 def Rot_x(r):
-    R_x = Matrix([[ 1,      0,       0],
+    r_x = Matrix([[ 1,      0,       0],
                   [ 0, cos(r), -sin(r)],
                   [ 0, sin(r),  cos(r)]])
-    return(R_x)
-
+    return(r_x)
 
 # Rotation (pitch)
 def Rot_y(p):
-    R_y = Matrix([[  cos(p), 0, sin(p)],
+    r_y = Matrix([[  cos(p), 0, sin(p)],
                   [       0, 1,      0],
                   [ -sin(p), 0, cos(p)]])
-    return(R_y)
-
+    return(r_y)
 
 # Rotation (yaw)
 def Rot_z(y):
-    R_z = Matrix([[ cos(y), -sin(y), 0],
+    r_z = Matrix([[ cos(y), -sin(y), 0],
                   [ sin(y),  cos(y), 0],
                   [      0,       0, 1]])
-    return(R_z)
+    return(r_z)
 
 
 def handle_calculate_IK(req):
@@ -60,47 +58,47 @@ def handle_calculate_IK(req):
     else:
         ### Your FK code here
         # Create symbols
-        # Representative variables of joint angles
-        joint1, joint2, joint3, joint4, joint5, joint6, joint7 = symbols('joint1:8')
+        # Joint angle variables
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
         # Link displacement variables
-        offset1, offset2, offset3, offset4, offset5, offset6, offset7 = symbols('offset1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
         # Link length variables
-        length0, length1, length2, length3, length4, length5, length6 = symbols('length0:7')
-        # Rotation angle variables
-        twist0, twist1, twist2, twist3, twist4, twist5, twist6 = symbols('twist0:7')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        # Twist angle variables
+        alpha0,alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
         # r: roll, p: pitch, y: yaw
         r, p, y = symbols('r p y')
         #
         #
         # Create Modified DH parameters
-        dh = {twist0:     0, length0:      0, offset1:  0.75,
-              twist1: -pi/2, length1:   0.35, offset2:     0, joint2: joint2-pi/2,
-              twist2:     0, length2:   1.25, offset3:     0,
-              twist3: -pi/2, length3: -0.054, offset4:   1.5,
-              twist4:  pi/2, length4:      0, offset5:     0,
-              twist5: -pi/2, length5:      0, offset6:     0,
-              twist6:     0, length6:      0, offset7: 0.303, joint7:           0}
+        dh = {alpha0:     0, a0:      0, d1:   0.75, q1:      q1,
+              alpha1: -pi/2, a1:   0.35, d2:      0, q2: q2-pi/2,
+              alpha2:     0, a2:   1.25, d3:      0, q3:      q3,
+              alpha3: -pi/2, a3: 0.0536, d4: 1.5014, q4:      q4,
+              alpha4:  pi/2, a4:      0, d5:      0, q5:      q5,
+              alpha5: -pi/2, a5:      0, d6:      0, q6:      q6,
+              alpha6:     0, a6:      0, d7:  0.303, q7:       0}
         #
         #
         # Define Modified DH Transformation matrix
         #
+        # Defined at the beginning
         #
         # Create individual transformation matrices
-        HT0_1 = HTF_Matrix(joint1, twist0, length0, offset1).subs(dh)
-        HT1_2 = HTF_Matrix(joint2, twist1, length1, offset2).subs(dh)
-        HT2_3 = HTF_Matrix(joint3, twist2, length2, offset3).subs(dh)
-        HT3_4 = HTF_Matrix(joint4, twist3, length3, offset4).subs(dh)
-        HT4_5 = HTF_Matrix(joint5, twist4, length4, offset5).subs(dh)
-        HT5_6 = HTF_Matrix(joint6, twist5, length5, offset6).subs(dh)
-        HT6_G = HTF_Matrix(joint7, twist6, length6, offset7).subs(dh)
+        HT0_1 = HTF_Matrix(alpha0, a0, d1, q1).subs(dh)
+        HT1_2 = HTF_Matrix(alpha1, a1, d1, q2).subs(dh)
+        HT2_3 = HTF_Matrix(alpha2, a2, d1, q3).subs(dh)
+        HT3_4 = HTF_Matrix(alpha3, a3, d1, q4).subs(dh)
+        HT4_5 = HTF_Matrix(alpha4, a4, d1, q5).subs(dh)
+        HT5_6 = HTF_Matrix(alpha5, a5, d1, q6).subs(dh)
+        HT6_G = HTF_Matrix(alpha6, a6, d1, q7).subs(dh)
         
         # Matrix of the transformations
         HT0_G = HT0_1 * HT1_2 * HT2_3 * HT3_4 * HT4_5 * HT5_6 * HT6_G 
         #
         #
         # Extract rotation matrices from the transformation matrices
-        R_corr = R_z(pi) * R_y(-pi/2)
-        R_rpy = R_z(y) * R_y(p) * R_x(r)
+        rot_rpy = Rot_x(r) * Rot_y(p) * Rot_z(y)
         #
         #
         ###
@@ -124,28 +122,30 @@ def handle_calculate_IK(req):
             
             ### Your IK code here
             # Compensate for rotation discrepancy between DH parameters and Gazebo
-            Rot_rpy = Rot_rpy.evalf(subs={r: roll, p: pitch, y: yaw})
-            R0_6 = Rot_rpy * Rot_corr
+            rot_corr = Rot_z.subs(y, pi) * Rot_y.subs(p, -pi/2)
+            rot_rpy = rot_rpy * rot_corr
+            rot_rpy = rot_rpy.subs({'r': roll, 'p': pitch, 'y': yaw})
             #
             #
             # Calculate joint angles using Geometric IK method
-            d_7 = dh[offset7]
-            wx = pos_x - (d_7 * R0_6[0,2])
-            wy = pos_y - (d_7 * R0_6[1,2])
-            wz = pos_z - (d_7 * R0_6[2,2])
+            d_7 = dh[d7]
+            wx = px - (d_7 * rot_rpy[0,2])
+            wy = py - (d_7 * rot_rpy[1,2])
+            wz = pz - (d_7 * rot_rpy[2,2])
             
             # Leveraging DH distances and offsets
-            d_1 = dh[offset1]
-            d_4 = dh[offset4]
-            a_1 = dh[length1]
-            a_2 = dh[length2]
-            a_3 = dh[length3]
+            d_1 = dh[d1]
+            d_4 = dh[d4]
+            a_1 = dh[a1]
+            a_2 = dh[a2]
+            a_3 = dh[a3]
             
             # For the evaluation of the angles we apply the law of cosine
             # Calculating theta 1 to 3
             r = sqrt(wx**2 + wy**2) - a_1
             s = wz - d_1
             
+            # Use of the cosine law
             s_a = sqrt(a_3**2 + d_4**2)
             s_b = sqrt(s**2 + r**2)
             s_c = a_2
@@ -157,23 +157,20 @@ def handle_calculate_IK(req):
             theta2 = (pi/2) - alpha - atan2(s, r)
             theta3 = (pi/2) - beta - atan2(-a_3, d_4)
             
+            # Calculating Euler angles from orientation
             # Calculating theta 4 to 6
-            R0_3 = HT0_1 * HT1_2 * HT2_3
-            R0_3 = R0_3[0:3, 0:3]
-            R0_3 = R0_3.evalf(subs={joint1: theta1, joint2: theta2, joint3: theta3})
-            R3_6 = R0_3.inv("ADJ") * R0_6 
+            R0_3 = HT0_1[0:3, 0:3] * HT1_2[0:3, 0:3] * HT2_3[0:3, 0:3]
+            R0_3 = R0_3.evalf(subs={'q1': theta1, 'q2': theta2, 'q3': theta3})
+            R3_6 = R0_3.HT * rot_rpy
             
-            r12 = R3_6[0, 1]
-            r13 = R3_6[0, 2]
-            r21 = R3_6[1, 0]
-            r22 = R3_6[1, 1]
-            r23 = R3_6[1, 2]
-            r32 = R3_6[2, 1]
-            r33 = R3_6[2, 2]
-            
-            theta4 = atan2(r33, -r13)
-            theta5 = atan2(sqrt(r13**2 + r33**2), r23)
-            theta6 = atan2(-r22, r21)
+            theta5 = atan2(sqrt(R3_6[0, 2]**2 + R3_6[2, 2]**2), R3_6[1, 2])
+            # Choosing between multiple possible solutions:
+            if sin(theta5) < 0:
+                theta4 = atan2(-R3_6[2, 2],  R3_6[0, 2])
+                theta6 = atan2( R3_6[1, 1], -R3_6[1, 0])
+            else:
+                theta4 = atan2( R3_6[2, 2], -R3_6[0, 2])
+                theta6 = atan2(-R3_6[1, 1],  R3_6[1, 0])
             #
             #
             ###
